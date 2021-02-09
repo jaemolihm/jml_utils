321ecb3c850eb5f56e4a695cbf96a5fc351d617c
Jae-Mo Lihm <jaemolim96@gmail.com>
2021-02-09 13:49:23 +0900

    Update qe_parse_energy.py. Parse occupations.

--- qe_parse_energy.py
+++ qe_parse_energy.py
@@ -28,28 +28,34 @@ def parse_energy(xmlfile):
     except KeyError:
         nk = find_value_int(root, 'nks')
 
-    weight = []
+    weight = np.zeros((nk,))
+    xk = np.zeros((3, nk))
+    energy = np.zeros((nbnd, nk))
+    occupations = np.zeros((nbnd, nk))
+    ik = 0
     for supitem in root.iter('ks_energies'):
         for item in supitem.iter('k_point'):
-            weight += [float(item.items()[0][1])]
-    assert len(weight) == nk
-    weight = np.array(weight)
-
-    ik = 0
-    energy = np.zeros((nbnd, nk))
-    for item in root.iter('eigenvalues'):
-        energy[:,ik] = [float(x) for x in item.text.split()]
+            weight[ik] += float(item.items()[0][1])
+            xk[:, ik] = [float(x) for x in item.text.split()]
+        for item in supitem.iter('eigenvalues'):
+            energy[:, ik] = [float(x) for x in item.text.split()]
+        for item in supitem.iter('occupations'):
+            occupations[:, ik] = [float(x) for x in item.text.split()]
         ik += 1
+    assert ik == nk
 
     if is_lsda:
         energy_lsda = np.zeros((nbnd//2, nk, 2))
         energy_lsda[:,:,0] = energy[:nbnd//2,:]
         energy_lsda[:,:,1] = energy[nbnd//2:,:]
+        occupations_lsda = np.zeros((nbnd//2, nk, 2))
+        occupations_lsda[:,:,0] = occupations[:nbnd//2,:]
+        occupations_lsda[:,:,1] = occupations[nbnd//2:,:]
 
     if is_lsda:
-        return is_lsda, nbnd, nk, weight, energy_lsda
+        return is_lsda, nbnd, nk, weight, energy_lsda, occupations_lsda, xk
     else:
-        return is_lsda, nbnd, nk, weight, energy
+        return is_lsda, nbnd, nk, weight, energy, occupations, xk
 
 if __name__ == '__main__':
     print(sys.argv)
@@ -63,11 +69,10 @@ if __name__ == '__main__':
     #xmlfile = outdir + '/' + prefix + '.xml'
     xmlfile = os.path.join(outdir, prefix + '.save', 'data-file-schema.xml')
 
-    is_lsda, nbnd, nk, weight, energy = parse_energy(xmlfile)
+    is_lsda, nbnd, nk, weight, energy, occupations, xk = parse_energy(xmlfile)
 
     print("Is LSDA calculation: ", is_lsda)
     print("number of bands: ", nbnd)
     print("number of k points: ", nk)
 
-    np.save('weight_' + prefix, weight)
     np.save('energy_' + prefix, energy)
